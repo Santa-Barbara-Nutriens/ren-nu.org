d8b4908e70d9ed27c8daad67079c10758dc2da53
manfred messing <mm@mm-ing.de>
2026-08-04 17:18:27 +0200

    Update hero image and refactor main function for webhook interaction

--- ren-nu-sales-funnel.ts
+++ ren-nu-sales-funnel.ts
@@ -6,20 +6,46 @@
  * to the underlying script/flow
  * @param token {string} User token from User Settings page
  */
-export async function main(
-    webhookUrl: string = "https://app.windmill.dev/api...",
-    scriptArgs = { "arg1": "first name", "arg2": "last name", "arg3": "email", "arg4": "phone number" },
-    token: string = "supersecret",
-) {
-    const options = {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(scriptArgs),
-    };
-    const response = await fetch(webhookUrl, options);
-    const data = await response.json();
+// export async function main(
+//     webhookUrl: string = "https://app.windmill.dev/api...",
+//     scriptArgs = { "arg1": "first name", "arg2": "last name", "arg3": "email", "arg4": "phone number" },
+//     token: string = "supersecret",
+// ) {
+//     const options = {
+//         method: "POST",
+//         headers: {
+//             "Authorization": `Bearer ${token}`,
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(scriptArgs),
+//     };
+//     const response = await fetch(webhookUrl, options);
+//     const data = await response.json();
+//     return data;
+// }
+
+
+export async function main() {
+    const jobTriggerResponse = await triggerJob();
+    const data = await jobTriggerResponse.json();
     return data;
+}
+
+async function triggerJob() {
+    const body = JSON.stringify({
+        "email": "",
+        "first_name": "",
+        "last_name": "",
+        "phone_number": ""
+    });
+    const endpoint = `https://app.windmill.dev/api/w/sbn-dev/jobs/run_wait_result/p/f/leads/submit_interest`;
+
+    return await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer <tvCyPtjhzjk389OVDFdXCIitgEhpCwbp>"
+        },
+        body
+    });
 }
